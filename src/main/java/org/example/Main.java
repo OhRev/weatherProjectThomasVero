ad0b232c7247586eb248654c080ed3d897a840d1
OhRev <121680448+OhRev@users.noreply.github.com>
2023-01-05 15:57:28 +0100

    Implementation des methodes de la classes temperatureDao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,12 @@ import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Date;
 import java.sql.SQLOutput;
 import java.util.concurrent.ExecutionException;
+
+import org.example.dao.TemperatureDao;
+import org.example.model.Temperature;
 import org.json.JSONObject;
 
 public class Main {
@@ -14,24 +18,30 @@ public class Main {
             throws URISyntaxException, ExecutionException, InterruptedException {
         // Appel api
 
-        URI line_api_url = new URI("https://api.openweathermap.org/data/2.5/weather?lat=35.6839" +
-                "&lon=139.7744&appid=8118ed6ee68db2debfaaa5a44c832918");
+//        URI line_api_url = new URI("https://api.openweathermap.org/data/2.5/weather?lat=35.6839" +
+//                "&lon=139.7744&appid=8118ed6ee68db2debfaaa5a44c832918");
+//
+//        // creation client (objet qui permet de se connecter à un url)
+//        HttpClient client = HttpClient.newBuilder().build();
+//
+//        // creation requete http a partir de l'uri
+//        HttpRequest request = HttpRequest.newBuilder().uri(line_api_url).GET().build();
+//
+//        // demande au client de faire la requete
+//        JSONObject result = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(HttpResponse::body).thenApply(JSONObject::new).get();
+//        JSONObject main = result.getJSONObject("main");
+//        float temp = main.getFloat("temp");
+//
+//
+//        System.out.println(main);
+//        System.out.println(temp);
+        Date date = new Date(114,8,20);
+        Temperature temperature = new Temperature(date, "Paris", 0.0f);
+        TemperatureDao tempDao = new TemperatureDao();
+        tempDao.create(temperature);
+        System.out.println(tempDao.readById(4));
 
-        // creation client (objet qui permet de se connecter à un url)
-        HttpClient client = HttpClient.newBuilder().build();
-
-        // creation requete http a partir de l'uri
-        HttpRequest request = HttpRequest.newBuilder().uri(line_api_url).GET().build();
-
-        // demande au client de faire la requete
-        JSONObject result = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body).thenApply(JSONObject::new).get();
-        JSONObject main = result.getJSONObject("main");
-        float temp = main.getFloat("temp");
-
-
-        System.out.println(main);
-        System.out.println(temp);
 
     }
 }
--- src/main/java/org/example/dao/GenericDao.java
+++ src/main/java/org/example/dao/GenericDao.java
@@ -6,6 +6,6 @@ public interface GenericDao <Entity,PK>{
     List<Entity> readAll(); // méthode qui retourne toutes les données de  la table
     Entity readById(PK id); // retourne un seul tuple selon l'identifiant
     Entity update(Entity entity); // modifie un tuple d'une table
-    boolean delete (PK id); // supprime un tuple de la table selon l'identifiant
+    boolean delete(PK id); // supprime un tuple de la table selon l'identifiant
 
 }
