f077f95ae838de685cd564db298ac7cf9516f8f8
OhRev <121680448+OhRev@users.noreply.github.com>
2023-01-05 12:00:20 +0100

    connexion API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,43 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.*;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.sql.SQLOutput;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static <JSONObject> void main(String[] args) throws URISyntaxException, ExecutionException, InterruptedException {
+        // Appel api
+
+        URI line_api_url = new URI("https://api.openweathermap.org/data/2.5/weather?lat=35.6839" +
+                "&lon=139.7744&appid=8118ed6ee68db2debfaaa5a44c832918");
+
+        // creation client (objet qui permet de se connecter à un url)
+        HttpClient client = HttpClient.newBuilder().build();
+
+        // creation requete http a partir de l'uri
+        HttpRequest request = HttpRequest.newBuilder().uri(line_api_url).GET().build();
+
+        // demande au client de faire la requete
+//        JSONObject result = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(HttpResponse::body).get();
+//
+//        JSONObject main = result.getJSONObject("main");
+//        float temp = main.getFloat("temp");
+
+//        JSONObject result =  client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(HttpResponse::body).get();
+//        float temp = result.getJSONObject("main").getFloat("temp");
+
+        String result = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+               .thenApply(HttpResponse::body).get();
+//        System.out.println(result);
+//        System.out.println(temp);
+
+
+
     }
 }
